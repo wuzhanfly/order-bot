2397386f473d746dcd732ca79c00ead0cf920dfd
wuzhanfly <wuzhanfly@163.com>
2022-05-23 11:11:22 +0800

    dodo

--- work/doWork.go
+++ work/doWork.go
@@ -20,7 +20,28 @@ import (
 var (
 	carpool chan map[string]bool
 	wg      sync.WaitGroup
+	res     map[string]interface{}
 )
+
+type ImportCar struct {
+	CarPath string `json:"carPath"`
+	Import  int64  `json:"import"`
+	Root    string `json:"root"`
+}
+type CommPCar struct {
+	CarPath string `json:"carPath"`
+	CID     string `json:"cid"`
+	Size    string `json:"size"`
+}
+
+type RES struct {
+	CarPath    string `json:"carPath"`
+	OriginPath string `json:"originPath"`
+	Import     int64  `json:"import"`
+	Root       string `json:"rootCid"`
+	CID        string `json:"cid"`
+	Size       string `json:"size"`
+}
 
 func DoWork(ctx context.Context, inputPath, outPath string) {
 	var carmap map[string]bool
@@ -75,6 +96,7 @@ func DoWorkCar(ctx context.Context, path string) {
 		if err != nil {
 			log.Println("ClientImport is err:", err)
 		}
+
 	}()
 	go func() {
 		err, _ := ClientCommP(ctx, path)
@@ -83,26 +105,29 @@ func DoWorkCar(ctx context.Context, path string) {
 
 		}
 	}()
+
 	wg.Wait()
 
 }
 
-func ClientImport(ctx context.Context, path string) (error, string) {
+func ClientImport(ctx context.Context, path string) (error, []ImportCar) {
 	defer func() {
 		if err := recover(); err != nil {
 			log.Fatalf("ClientImport  panic: %v, stack: %s", err, debug.Stack())
 		}
 	}()
+	var cars []ImportCar
 	pathname, err := GetAllFile(path)
 	if err != nil {
 		log.Fatalf("get file is failed:%v", err)
 	}
 	for _, s := range pathname {
+		var car RES
 		absPath, err := filepath.Abs(s)
 		if err != nil {
 			log.Fatalf("get file abs is failed:%v", err)
 
-			return err, ""
+			return err, cars
 		}
 
 		ref := lapi.FileRef{
@@ -111,54 +136,69 @@ func ClientImport(ctx context.Context, path string) (error, string) {
 		}
 		c, err := lotuss.Node().ClientImport(ctx, ref)
 		if err != nil {
-			return err, ""
+			return err, cars
 		}
 		encoder, err := GetCidEncoder(ctx)
 
 		if err != nil {
 			log.Fatalf("encoder file is failed:%v", err)
-			return err, ""
+			return err, cars
 		}
 		fmt.Printf("Import %d, Root ", c.ImportID)
 		fmt.Println(encoder.Encode(c.Root))
 
+		car.CarPath = absPath
+		car.Import = int64(c.ImportID)
+		car.Root = encoder.Encode(c.Root)
+		res[absPath] = &car
 	}
 	wg.Done()
 
-	return nil, ""
+	return nil, cars
 }
 
-func ClientCommP(ctx context.Context, path string) (error, string) {
+func ClientCommP(ctx context.Context, path string) (error, []CommPCar) {
 	defer func() {
 		if err := recover(); err != nil {
 			log.Fatalf("ClientCommP  panic: %v, stack: %s", err, debug.Stack())
 		}
 	}()
+	var CommPs []CommPCar
 	pathname, err := GetAllFile(path)
 	if err != nil {
 		log.Fatalf("get file is failed:%v", err)
-		return nil, ""
+		return nil, CommPs
 	}
 	for _, s := range pathname {
+		var ccar CommPCar
+
 		if !strings.Contains(s, ".car") {
 			continue
 		}
 		ret, err := lotuss.Node().ClientCalcCommP(ctx, s)
 		if err != nil {
-			return err, ""
+			return err, CommPs
 		}
 		encoder, err := GetCidEncoder(ctx)
 		if err != nil {
 			log.Fatalf("encoder file is failed:%v", err)
-			return err, ""
+			return err, CommPs
 		}
-
+		ccar.CID = encoder.Encode(ret.Root)
+		ccar.Size = types.SizeStr(types.NewInt(uint64(ret.Size)))
 		fmt.Println("CID: ", encoder.Encode(ret.Root))
 		fmt.Println("Piece size: ", types.SizeStr(types.NewInt(uint64(ret.Size))))
+		CommPs = append(CommPs, ccar)
+	}
+	for _, p := range CommPs {
+		if _, ok := res[p.CarPath]; ok {
+			res[p.Size] = p.Size
+			res[p.CID] = p.CID
+		}
 	}
 	wg.Done()
 
-	return nil, ""
+	return nil, CommPs
 }
 
 // GetAllFile 递归获取指定目录下的所有文件名
@@ -194,7 +234,6 @@ func GetAllFile(pathname string) ([]string, error) {
 // the default (Base32) encoder if not.
 
 func GetCidEncoder(ctx context.Context) (cidenc.Encoder, error) {
-
 	e := cidenc.Encoder{Base: multibase.MustNewEncoder(multibase.Base32)}
 
 	return e, nil
